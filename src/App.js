2ea78276d3e36e3cf277f978ba678c67d9830cd1
NhuTanCS <tan.dinh1710@hcmut.edu.vn>
2021-03-27 00:01:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardList from './CardList';
+//import SearchBox from '../components/SearchBox';
+//import Scroll from '../components/Scroll';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
 
+  constructor() {
+    super()
+    this.state = {
+      robots: []
+    }
+  }
+
+  componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
+  }
+  render(){
+    const {robots} = this.state;
+    return(
+      <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <CardList robots={robots} />
+      </div>
+    )
+  }
+}
 export default App;
